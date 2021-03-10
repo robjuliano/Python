423e9378af00b5a9d911cf6cd38262b5ea6b9204
robjuliano <robsonj2011@gmail.com>
2021-03-10 15:13:11 -0300

    Initial

--- volume.py
+++ volume.py
@@ -2,12 +2,12 @@ largura = float(input("Qual a largura ? :"))
 altura = float(input("Qual a altura ? :"))
 pi = 3.14
 volume = 0
-farinha =1.66
+farinha = 1.66
 acucar = 1.18
 largura = largura / 2
 
 
-largura = largura *largura
+largura = largura * largura
 
 largura = largura * pi
 
@@ -16,8 +16,8 @@ volume = largura * altura
 farinha = volume / farinha
 acucar = volume / acucar
 
-print("Volume :", round(volume,2))
+print("Volume :", round(volume, 2))
 
-print("Peso em gramas Farinha", round(farinha,2))
+print("Peso em gramas Farinha", round(farinha, 2))
 
-print("Peso em gramas Açucar", round(acucar,2))
+print("Peso em gramas Açucar", round(acucar, 2))
